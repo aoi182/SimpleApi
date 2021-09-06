32132ed180222f00d8f767bcacc288aae8d64191
adonis cruz villarraga <acruz@calcoastcu.org>
2021-09-06 14:40:30 -0500

    enable swagger

--- src/SimpleApi/Startup.cs
+++ src/SimpleApi/Startup.cs
@@ -40,9 +40,10 @@ namespace SimpleApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SimpleApi v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SimpleApi v1"));
 
             app.UseHttpsRedirection();
 
